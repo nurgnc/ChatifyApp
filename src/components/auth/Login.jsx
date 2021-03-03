c3bedc5e633726275b37c5d4ef273daeeff38f8a
nurgnc <gencbedriyenur@gmail.com>
2021-03-03 19:20:02 +0300

    created components and route

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { From, Segment, Button, Grid, Message } from 'semantic-ui-react';
+import styles from "./login.module.css";
+
+const Login = () => {
+    return (
+        <Grid textAlign="center" verticalAlign="middle" className={styles.container}>
+            
+        </Grid>
+    )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+//Styles
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Components
+import SignUp from "./components/auth/SignUp";
+import Login from "./components/auth/Login";
+
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={Login} />
+    </Switch>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Root />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
