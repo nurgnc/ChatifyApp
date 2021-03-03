1ab4ed9bf7d9eb9c1f34c85ac287c44c7a43261f
nurgnc <gencbedriyenur@gmail.com>
2021-03-03 19:46:35 +0300

    login component has been developed

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { From, Segment, Button, Grid, Message } from 'semantic-ui-react';
+import { Form, Segment, Button, Grid, Message } from 'semantic-ui-react';
 import styles from "./login.module.css";
 
 const Login = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
+
     return (
         <Grid textAlign="center" verticalAlign="middle" className={styles.container}>
-            
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <h1 className={styles.formHeader}>Chatify
+                <span><div className="io"></div></span>
+                </h1>
+
+                <Form size="large" className={styles.form} onSubmit={handleSubmit}>
+                    <Segment>
+                        <Form.Input fluid icon="mail" iconPosition="left" name="email" placeholder="Email" type="email" />
+                        <Form.Input fluid icon="lock" iconPosition="left" name="password" placeholder="Parola" type="password" />
+
+                        <Button color="purple" fluid size="large"> Giriş Yap </Button>
+                    </Segment>
+                </Form>
+            </Grid.Column>
         </Grid>
     )
 }
 
-export default Login
+export default Login;
